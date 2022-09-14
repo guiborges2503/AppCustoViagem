e3f3cc9955a9a961e6c0c7c3cad72e55ddda428e
Guilherme <guilhermetravain1@gmail.com>
2022-09-14 15:24:53 -0300

    a

---
+++ AppCustoViagem/AppCustoViagem/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+using AppCustoViagem.Model;
+using System.Threading.Tasks;
+
+namespace AppCustoViagem.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLiteDatabaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Viagem>().Wait();
+            _db.CreateTableAsync<Pedagio>().Wait();
+        }
+        public Task<List<Viagem>> GetAllViagens()
+        {
+     
+
+        public Task<Viagem> GetViagemById(int id)
+        {
+            return _db.Table<Viagem>().FirstAsync(i => i.Id == id);
+        }
+
+        public Task<Viagem> InsertViagem(Viagem v)
+        {
+            _db.InsertAsync(v);
+
+            return _db.Table<Viagem>().OrderByDescending(i => i.Id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> DeleteViagem(int id)
+        {
+            return _db.Table<Viagem>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Pedagio>> GetAllPedagios(int id_viagem)
+        {
+            return _db.Table<Pedagio>().OrderByDescending(i => i.Id).ToListAsync();
+        }
+
+        public Task<int> InsertPedagio(Pedagio p)
+        {
+            return _db.InsertAsync(p);
+        }
+    }
+}
--- AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
+++ AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
@@ -2,16 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-using SQLite;
-
 namespace AppCustoViagem.Model
 {
-    public class Pedagio
+    internal class Pedagio
     {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-        public int Id_Viagem { get; set; }
-        public string Localizacao { get; set; }
-        public double Valor { get; set; }
     }
 }
