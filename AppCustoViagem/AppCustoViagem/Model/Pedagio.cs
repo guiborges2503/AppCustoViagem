026dacc897c87d32a77fa794884170edf645e938
Guilherme <guilhermetravain1@gmail.com>
2022-09-14 15:20:37 -0300

    ll

--- AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
+++ AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+using SQLite;
+
 namespace AppCustoViagem.Model
 {
-    internal class Pedagio
+    public class Pedagio
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int Id_Viagem { get; set; }
+        public string Localizacao { get; set; }
+        public double Valor { get; set; }
     }
 }
--- AppCustoViagem/AppCustoViagem/Model/Viagem.cs
+++ AppCustoViagem/AppCustoViagem/Model/Viagem.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace AppCustoViagem.Model
 {
-    internal class Viagem
+    public class Viagem
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+
+        public double Distancia { get; set; }
+        public double Consumo { get; set; }
+        public double Valor_Litro_Combustivel { get; set; }
+
+        public double Total_Viagem
+        {
+            get { return (Distancia / Consumo) * Valor_Litro_Combustivel; }
+        }
     }
 }
